566a3b6f8e059cc81f188dbad04bbaddfb2c3e10
Piotrek <Piotr.karys96@gmail.com>
2024-07-28 22:24:52 +0200

    final version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import PrivateRoute from "./PrivateRoute";
+import RestrictedRoute from "./RestrictedRoute";
+import { refreshUser } from "../redux/auth/operations";
+import { useAuth } from "../hooks/useAuth";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Contacts from "../pages/Contacts/Contacts";
+import { Box } from "@chakra-ui/react";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  if (isRefreshing) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      <h1>(Vite)</h1>
-    </>
+    <Box
+      minHeight="100vh"
+      bgGradient="linear(to-r, teal.300, blue.500)"
+      p={0}
+      m={0}>
+      {" "}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<Register />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<Contacts />} />
+            }
+          />
+        </Route>
+      </Routes>
+    </Box>
   );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+import App from "./components/App";
+import Notiflix from "notiflix";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
+import "./index.css";
+
+Notiflix.Notify.init({
+  width: "280px",
+  position: "right-top",
+  distance: "10px",
+  opacity: 1,
+  timeout: 3000,
+  clickToClose: true,
+  pauseOnHover: true,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <ChakraProvider theme={theme}>
+            <App />
+          </ChakraProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 );
