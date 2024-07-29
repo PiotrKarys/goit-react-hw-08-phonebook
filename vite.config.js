c6d7d2647a807b6b57ba5e87de3680c12d4c6b2f
Piotrek <Piotr.karys96@gmail.com>
2024-07-29 11:39:23 +0200

    base upd

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/goit-react-hw-08-phonebook/",
   build: {
-    outDir: "./dist",
+    outDir: "dist",
     minify: "esbuild",
   },
 });
